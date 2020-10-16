c8dc6da738517b8cb1865430f47d6a798de6db13
Cam C <ckc.cameron@gmail.com>
2020-10-16 09:37:32 -0700

    Create SearchFacade with parks method
    * maps each park using Park PORO to aggregate attributes needed for 
    display within view

---
+++ app/facades/search_facade.rb
@@ -0,0 +1,15 @@
+class SearchFacade
+  def self.parks(state)
+    parks = service.get_parks(state)[:data]
+    parks.map { |park_info| park(park_info)}
+    binding.pry
+  end
+
+  def self.service
+    ParkService.new
+  end
+
+  def self.park(park_info)
+    Park.new(park_info)
+  end
+end
